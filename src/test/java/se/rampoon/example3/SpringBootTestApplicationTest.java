7ecb70662972a98effe9bbea221befaa9b49f4de
Mats Javehed <mats.javehed@gmail.com>
2024-04-21 17:09:36 +0200

    MATS-0002 Use Java17

--- src/test/java/se/rampoon/example3/SpringBootTestApplicationTest.java
+++ src/test/java/se/rampoon/example3/SpringBootTestApplicationTest.java
@@ -1,11 +1,16 @@
 package se.rampoon.example3;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
+//@RunWith(SpringRunner.class)
+@SpringBootTest
 public class SpringBootTestApplicationTest
 {
     /**
